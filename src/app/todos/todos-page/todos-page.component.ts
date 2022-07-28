3df8b76f778bd46353690e559c7453e8e04fdb9b
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-28 11:02:19 +0400

    feat: Create TodoModule

---
+++ src/app/todos/todos-page/todos-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-page',
+  templateUrl: './todos-page.component.html',
+})
+export class TodosPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/todos/todos.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodosPageComponent } from './todos-page/todos-page.component';
+
+@NgModule({
+  declarations: [TodosPageComponent],
+  imports: [CommonModule],
+})
+export class TodosModule {}
