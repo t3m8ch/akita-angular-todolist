6590e01b96084e05039da757b2a3b2b700a2da55
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-28 11:02:36 +0400

    feat: Add routes for /todos and /

--- src/app/routing/routes.ts
+++ src/app/routing/routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TodosPageComponent } from '../todos/todos-page/todos-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'todos', component: TodosPageComponent },
+  { path: '', redirectTo: '/todos', pathMatch: 'full' },
+];
